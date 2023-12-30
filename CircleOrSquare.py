3e66c53e92fb0f14855cb2d2b1241227149d1659
izzybeetschen <114227602+izzybeetschen@users.noreply.github.com>
2023-12-30 23:05:32 +0000

    Attempt 1

--- CircleOrSquare.py
+++ CircleOrSquare.py
@@ -1,3 +1,11 @@
 """ Given the radius of a circle and the area of a square, return True if the circumference of the circle is greater
 than the square's perimeter and False if the square's perimeter is greater than the circumference of the circle. """
+import math
 
+def circle_or_square(rad, area):
+    circumference = 2 * 3.14 * rad
+    perimeter = math.sqrt(area) * 4
+    if circumference > perimeter:
+        return True
+    else:
+        return False
