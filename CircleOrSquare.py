c279eb13d5477bf87c4e708be90bccf2b4488d84
izzybeetschen <114227602+izzybeetschen@users.noreply.github.com>
2023-12-30 22:59:52 +0000

    Initial commit

---
+++ CircleOrSquare.py
@@ -0,0 +1,3 @@
+""" Given the radius of a circle and the area of a square, return True if the circumference of the circle is greater
+than the square's perimeter and False if the square's perimeter is greater than the circumference of the circle. """
+
